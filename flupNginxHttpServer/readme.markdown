02f02f9019baff5e9b09acf9650e99859ee591d8
HanyuuLu <HanyuuFurude@outlook.com>
2019-07-03 15:12:52 +0800

    201907031512

---
+++ flupNginxHttpServer/readme.markdown
@@ -0,0 +1,4 @@
+# Nginx + flup
+* 使用Nginx接受请求
+* 使用flup处理信息
+* 
--- flupNginxHttpServer/server.py
+++ flupNginxHttpServer/server.py
@@ -1,6 +1,7 @@
-from flup.server.fcgi import WSGIServer
-def myapp(environ,start_response):
-	start_response('200 OK',[('Content-Type','text/plain')])
-	return ['hello world!\n']
-if __name__=='__main__':
-	WCGIServer(myapp,bindAddress('localhost',8000)).run()
+from flup.server.fcgi import WSGIServer 
+def res(environ, start_response): 
+	start_response('200 OK', [('Content-Type', 'text/plain')]) 
+	return ['Hello World!\n'] 
+if __name__ == '__main__': 
+	WSGIServer(res,bindAddress=('127.0.0.1',8008)).run()
+
