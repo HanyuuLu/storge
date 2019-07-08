b52466cd3a70e2b36da57a1efa542d9f86e415db
HanyuuLu <HanyuuFurude@outlook.com>
2019-07-08 19:07:25 +0800

    201907081907

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
+import { SSL_OP_NO_TICKET } from 'constants';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hanyuu</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p id="example">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://hanyuufurude.github.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hanyuu Furude's personal web page.
         </a>
       </header>
     </div>
   );
 }
-
+function tick()
+{
+  const element = (
+    <div>
+      <h2>现在是 {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('example')
+  );
+}
+setInterval(tick,1000);
 export default App;
