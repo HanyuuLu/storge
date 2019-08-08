07f0a771f0217f023944bc6de08560c6b8c1bdf5
HanyuuLu <HanyuuFurude@outlook.com>
2019-08-08 14:50:14 +0800

    201908081450

--- react-native/App.js
+++ react-native/App.js
@@ -1,112 +1,90 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-import React, { Component } from 'react';
-import { Text, View, Image, AppRegistry, StyleSheet, TextInput, Button, Alert, ScrollView } from 'react-native';
-import { validate } from '@babel/types';
-var MOCKED_MOVIES_DATA = [
-    {
-        title: "标题",
-        year: "2015",
-        posters: { thumbnail: "https://hanyuufurude.github.io/Others/positionMesher/1564911063046.png" }
-    }
-];
+
+import React, { Component } from "react";
+
+import { Image, FlatList, StyleSheet, Text, View } from "react-native";
+
 var REQUEST_URL =
     "https://raw.githubusercontent.com/facebook/react-native/0.51-stable/docs/MoviesExample.json";
-export default class Hanyuu extends Component
+
+export default class SampleAppMovies extends Component
 {
     constructor(props)
     {
         super(props);
         this.state = {
-            movies: null,
-        }
-        this.fetchdata = this.fetchdata.bind(this);
+            data: [],
+            loaded: false
+        };
+        // 在ES6中，如果在自定义的函数里使用了this关键字，则需要对其进行“绑定”操作，否则this的指向会变为空
+        // 像下面这行代码一样，在constructor中使用bind是其中一种做法（还有一些其他做法，如使用箭头函数等）
+        this.fetchData = this.fetchData.bind(this);
     }
+
     componentDidMount()
     {
-        this.fetchdata();
+        this.fetchData();
     }
-    fetchdata()
+
+    fetchData()
     {
         fetch(REQUEST_URL)
-            .then((response) => response.json())
-            .then((responseData) =>
+            .then(response => response.json())
+            .then(responseData =>
             {
-                this.setState(
-                    { movies: responseData.movies, }
-                    );
+                // 注意，这里使用了this关键字，为了保证this在调用时仍然指向当前组件，我们需要对其进行“绑定”操作
+                this.setState({
+                    data: this.state.data.concat(responseData.movies),
+                    loaded: true
+                });
             });
     }
+
     render()
     {
-        if (!this.state.movies)
+        if (!this.state.loaded)
         {
             return this.renderLoadingView();
         }
-        var movie = this.state.movies[0];
-        return this.renderMovie(movie);
 
-        // var movie = MOCKED_MOVIES_DATA[0];
-        // return (
-        //     <ScrollView>
-        //         <View style={styles.container}>
-        //             <Image
-        //                 style={styles.thumbnail}
-        //                 source={{ uri: movie.posters.thumbnail }}
-        //             />
-        //             <View style={styles.rightContainer}>
-
-        //                 <Text style={styles.title}>{movie.title}</Text>
-        //                 <Text style={styles.year}>{movie.year}</Text>
-        //             </View>
-        //         </View>
-        //     </ScrollView>
-        // );
+        return (
+            <FlatList
+                data={this.state.data}
+                renderItem={this.renderMovie}
+                style={styles.list}
+                keyExtractor={item => item.id}
+            />
+        );
     }
+
     renderLoadingView()
     {
         return (
             <View style={styles.container}>
-                <Text>
-                    正在加载电影数据……
-        </Text>
-            </View>
-        );
-    }
-    renderMovie(movie)
-    {
-        return (
-            <View style={styles.container}>
-                <Image
-                    source={{ uri: movie.posters.thumbnail }}
-                    style={styles.thumbnail}
-                />
-                <View style={styles.rightContainer}>
-                    <Text style={styles.title}>{movie.title}</Text>
-                    <Text style={styles.year}>{movie.year}</Text>
-                </View>
+                <Text>Loading movies...</Text>
             </View>
         );
     }
 
+    renderMovie({ item })
+    {
+        // { item }是一种“解构”写法，请阅读ES2015语法的相关文档
+        // item也是FlatList中固定的参数名，请阅读FlatList的相关文档
+        return (
+            <View style={styles.container}>
+                <Image
+                    source={{ uri: item.posters.thumbnail }}
+                    style={styles.thumbnail}
+                />
+                <View style={styles.rightContainer}>
+                    <Text style={styles.title}>{item.title}</Text>
+                    <Text style={styles.year}>{item.year}</Text>
+                </View>
+            </View>
+        );
+    }
 }
-const styles = StyleSheet.create({
-    title: {
-        fontSize: 20,
-        marginBottom: 8,
-        textAlign: "center",
-    },
-    year: {
-        textAlign: "center",
-    },
-    rightContainer: {
-        flex: 1
-    },
+
+var styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: "row",
@@ -114,25 +92,23 @@ const styles = StyleSheet.create({
         alignItems: "center",
         backgroundColor: "#F5FCFF"
     },
+    rightContainer: {
+        flex: 1
+    },
+    title: {
+        fontSize: 20,
+        marginBottom: 8,
+        textAlign: "center"
+    },
+    year: {
+        textAlign: "center"
+    },
     thumbnail: {
         width: 53,
         height: 81
     },
-    icon: {
-
-    },
-    cyan: {
-        color: 'cyan',
-        fontSize: 30,
-    },
-    blue: {
-        color: 'blue',
-        fontSize: 30,
-    },
-    black:
-    {
-        fontSize: 40,
-    },
-    hc:
-        { fontSize: 25 }
+    list: {
+        paddingTop: 20,
+        backgroundColor: "#F5FCFF"
+    }
 });
