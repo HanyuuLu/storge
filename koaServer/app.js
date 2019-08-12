5bc6500eeef5b4a70340bc8be601e3f3286fffad
HanyuuLu <HanyuuFurude@outlook.com>
2019-08-12 17:36:13 +0800

    201908121736

--- koaServer/app.js
+++ koaServer/app.js
@@ -1,7 +1,27 @@
-const Koa = require('koa');
-const app = new Koa();
+const fs = require('fs');
+const path = require('path');
+const serve = require('koa-static');
+const koa = require('koa');
+const koaBodyParser = require('koa-bodyparser')
+const koaRouter =require('koa-router')();
+const app = new koa();
 
-//logger
+koaRouter.get('/hello/:name', async (ctx, next) =>
+{
+    var name = ctx.params.name;
+    ctx.response.body = `<h1>hi,${name}!</h1>`;
+    console.log("caught!");
+});
+koaRouter.get('/', async (ctx, next) =>
+{
+    var content = fs.readFileSync('.\\resource\\index.html');
+    ctx.response.body = content;
+    ctx.response.type = 'text/html';
+})
+koaRouter.post('/', async (ctx, next) =>
+{
+    ctx.response.body(ctx.)
+})
 app.use(async (ctx, next) =>
 {
     await next();
@@ -16,10 +36,15 @@ app.use(async (ctx, next) =>
     const ms = Date.now() - start;
     ctx.set('X-Response-Time', `${ms}ms`);
 })
-app.use(async ctx =>
-{
-    ctx.body = 'Hello World';
-    console.log(ctx.req.host);
-});
+app.use(koaBodyParser());
+app.use(koaRouter.routes());
+app.use(serve(path.join(__dirname + '\\resource')));
+// app.use(async (ctx,next) =>
+// {
+    //     ctx.body = ctx.request.path + '\n' + ctx.request.method;
+    //     // ctx.body = 'Hello World';
+    //     // console.log(ctx.req.host);
+    // });
 
-app.listen(3000);
+
+    app.listen(80);
