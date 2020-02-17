4cbc286562960658e5b36854409e2f1e2b33db38
HanyuuuLu <HanyuuFurude@outlook.com>
2020-02-18 01:35:08 +0800

    202002180135

--- kindle/ExtraServer/Server.ts
+++ kindle/ExtraServer/Server.ts
@@ -1,7 +1,22 @@
 import Koa from 'koa';
+import KoaRouter from 'koa-router'
+import koaBody from 'koa-body';
+import bodyParser from 'koa-bodyparser';
+import {fetchBilibiliLiveStatus} from './requester'
 const Server = new Koa();
+const Router = new KoaRouter();
 
-Server.use(async ctx => {
-    ctx.body = "Hanyuu Server";
+Server.use(koaBody({multipart:true}));
+Server.use(Router.routes());
+Server.use(bodyParser())
+Router.get('/api/kindle/query/bilibili/live/:roomid',async (ctx,next)=>
+{
+    const roomid = ctx.params.roomid;
+    ctx.response.body =  await fetchBilibiliLiveStatus(roomid)
+    ctx.response.type = "application/json"
+})
+Router.get('/api/kindle/query/wether:city',async (ctx,next)=>
+{
+
 })
 Server.listen(80); 
