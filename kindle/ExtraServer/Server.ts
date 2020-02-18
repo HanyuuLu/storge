c3cc463d51d75203220b1e24027322c15d848ea6
HanyuuuLu <HanyuuFurude@outlook.com>
2020-02-19 01:29:48 +0800

    202002190129

--- kindle/ExtraServer/Server.ts
+++ kindle/ExtraServer/Server.ts
@@ -2,21 +2,27 @@ import Koa from 'koa';
 import KoaRouter from 'koa-router'
 import koaBody from 'koa-body';
 import bodyParser from 'koa-bodyparser';
-import {fetchBilibiliLiveStatus} from './requester'
+import { fetchBilibiliLiveStatus, fetchBilibiliUserSpaceHistory } from './requester'
+import { bilibiliUserSpaceHistoryFileter } from './filter';
 const Server = new Koa();
 const Router = new KoaRouter();
 
-Server.use(koaBody({multipart:true}));
+Server.use(koaBody({ multipart: true }));
 Server.use(Router.routes());
 Server.use(bodyParser())
-Router.get('/api/kindle/query/bilibili/live/:roomid',async (ctx,next)=>
-{
+const baseURL = "/api"
+Router.get(`${baseURL}/bilibili/live/:roomid`, async (ctx, next) => {
     const roomid = ctx.params.roomid;
-    ctx.response.body =  await fetchBilibiliLiveStatus(roomid)
+    ctx.response.body = await fetchBilibiliLiveStatus(roomid)
     ctx.response.type = "application/json"
 })
-Router.get('/api/kindle/query/wether:city',async (ctx,next)=>
-{
+Router.get(`${baseURL}/bilibili/history/:uid`, async (ctx, next) => {
+    var res: any = await fetchBilibiliUserSpaceHistory(ctx.params.uid)
+    var list = bilibiliUserSpaceHistoryFileter(res)
+    ctx.response.body = list[0]
+    ctx.response.type = "application/json"
+})
+Router.get('/api/kindle/query/wether:city', async (ctx, next) => {
 
 })
 Server.listen(80); 
