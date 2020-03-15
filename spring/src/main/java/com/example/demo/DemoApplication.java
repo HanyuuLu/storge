43de972ebbb9119d45d7a5b2165bf32b5250790c
HanyuuuLu <HanyuuFurude@outlook.com>
2020-03-15 21:14:50 +0800

    202003152114

--- spring/src/main/java/com/example/demo/DemoApplication.java
+++ spring/src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,16 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
 
+	// private final AtomicLong counter = new AtomicLong();
 	public static void main(String[] args) {
-	SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-	return String.format("Hello %s!", name);
+		return String.format("Hello %s!", name);
 	}
+
 	@GetMapping("/")
 	public String index() {
-	return "Hanyuu's spring.";
+		return "Hanyuu's spring.";
+	}
+
+	@GetMapping("/greeting")
+	public User greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		// return new Greeting(counter.incrementAndGet(),String.format("Hi, %s",name));
+		return new User(1, String.format("Hi, %s", name));
 	}
 }
--- spring/src/main/java/com/example/demo/Hanyuu.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-public class Hanyuu{
-    private String message;
-    public  void setMessage(String message) {
-        this.message = message;
-        
-    }
-}
